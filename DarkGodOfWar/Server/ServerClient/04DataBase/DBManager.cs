70b703714bac969d994d0cffaa25de6accda8f97
LoriaRujoy <1306650725@qq.com>
2024-09-11 18:07:58 +0800

    完善查询获取玩家数据的QueryPlayerData()的功能

--- DarkGodOfWar/Server/ServerClient/04DataBase/DBManager.cs
+++ DarkGodOfWar/Server/ServerClient/04DataBase/DBManager.cs
@@ -11,6 +11,7 @@
 
 using MySql.Data.MySqlClient;
 using PEProtocol;
+using System;
 
 public class DBManager
 {
@@ -45,6 +46,60 @@ public class DBManager
     public PlayerData QueryPlayerData(string acct,string pass)
     {
         PlayerData playerData = null;
+        MySqlDataReader reader = null;//存储查询出来的数据
+        bool isNew = true;//区分新旧账号
+        try
+        {
+            //Sql查询语句：选择指令 查找的数据是什么，从哪里查找，选择条件
+            //其中：查找的数据用*表示，表示找出所有满足条件的数据；选择条件 这里不写的话，就是将表中所有数据列出来
+            //使用@变量方式，不把数据写死，后面使用AddWithValue()替换参数，进行获取、更新数据；
+            MySqlCommand cmd = new MySqlCommand("select * from account where acct = @acct", SqlConnection);
+            cmd.Parameters.AddWithValue("acct", acct);//获取acct对应的数据
+            reader = cmd.ExecuteReader();//执行Sql语句
+            if (reader.Read())//处理数据
+            {
+                isNew = false;//有旧账号
+                string _pass = reader.GetString("pass");
+                if (_pass.Equals(pass))
+                {
+                    playerData = new PlayerData//密码正确，返回玩家数据
+                    {
+                        id = reader.GetInt32("id"),
+                        name = reader.GetString("name"),
+                        lv = reader.GetInt32("level"),
+                        exp = reader.GetInt32("exp"),
+                        power = reader.GetInt32("power"),
+                        coin = reader.GetInt32("coin"),
+                        diamond = reader.GetInt32("diamond")
+                    };
+                }
+            }
+        }
+        catch (Exception e) { PECommon.Log("Query PlayerData By Acct&Pass Error：" + e, LogType.Error); }
+        finally
+        {
+            if (isNew)//没有旧账号，创建新账号
+            {
+                playerData = new PlayerData
+                {
+                    id = -1,
+                    name = "",
+                    lv = 1,
+                    exp = 0,
+                    power = 150,
+                    coin = 5000,
+                    diamond = 500
+                };
+                //新账号存到数据库，并将返回的id更新到playerData里
+                playerData.id = InsertNewAcctData(acct, pass, playerData);
+            }
+        }
         return playerData;
+    }
+
+    //将新账号数据存到数据库中
+    private int InsertNewAcctData(string acct,string pass,PlayerData pData)
+    {
+        return 0;
     }
 }
