3be9c0bbfd14aa8948919f98eaa803ae48b1528d
LoriaRujoy <1306650725@qq.com>
2024-09-12 22:41:18 +0800

    数据库数据表保存备份

---
+++ DarkGodOfWar/Sql/sql_darkgod.sql
@@ -0,0 +1,37 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : sql_darkgod
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2024-09-12 22:19:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `acct` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `level` int(11) NOT NULL,
+  `exp` int(11) NOT NULL,
+  `power` int(11) NOT NULL,
+  `coin` int(11) NOT NULL,
+  `diamond` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
