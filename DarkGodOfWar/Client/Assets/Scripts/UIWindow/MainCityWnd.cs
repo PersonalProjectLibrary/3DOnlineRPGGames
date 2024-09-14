d3d8f9e328bcbf7152127346c628faa1f4519c64
LoriaRujoy <1306650725@qq.com>
2024-09-14 16:16:54 +0800

    设置主城战力体力UI显示逻辑

--- DarkGodOfWar/Client/Assets/Scripts/UIWindow/MainCityWnd.cs
+++ DarkGodOfWar/Client/Assets/Scripts/UIWindow/MainCityWnd.cs
@@ -10,10 +10,53 @@
 #endregion
 
 
+using PEProtocol;
+using UnityEngine.UI;
+
 public class MainCityWnd : WindowRoot
 {
+    /// <summary>
+    /// 战斗力
+    /// </summary>
+    public Text txtFight;
+    /// <summary>
+    /// 体力
+    /// </summary>
+    public Text txtPower;
+    /// <summary>
+    /// 体力进度条
+    /// </summary>
+    public Image imgPowerPrg;
+    /// <summary>
+    /// 等级
+    /// </summary>
+    public Text txtLevel;
+    /// <summary>
+    /// 名字
+    /// </summary>
+    public Text txtName;
+    /// <summary>
+    /// 经验进度条
+    /// </summary>
+    public Text txtExpPrg;
+
     protected override void InitWnd()
     {
         base.InitWnd();
+        RefreshUI();
+    }
+
+    /// <summary>
+    /// 刷新主城UI界面
+    /// </summary>
+    private void RefreshUI()
+    {
+        PlayerData pData = GameRoot.Instance.PlayerData;//获取玩家数据
+
+        SetText(txtName, pData.name);
+        SetText(txtLevel, pData.lv);
+        SetText(txtFight, PECommon.GetFightByProps(pData));
+        SetText(txtPower, "体力:" + pData.power + "/" + PECommon.GetPowerLimit(pData.lv));
+        imgPowerPrg.fillAmount = pData.power * 1.0f / PECommon.GetPowerLimit(pData.lv);
     }
 }
--- DarkGodOfWar/Server/PEProtocol/PECommon.cs
+++ DarkGodOfWar/Server/PEProtocol/PECommon.cs
@@ -10,6 +10,7 @@
 #endregion
 
 using PENet;
+using PEProtocol;
 
 /// <summary>
 /// 消息等级
@@ -28,5 +29,29 @@ public class PECommon
     {
         LogLevel lv = (LogLevel)tp;
         PETool.LogMsg(msg, lv);
+    }
+
+    /// <summary>
+    /// 计算玩家战斗力
+    /// </summary>
+    /// <param name="pData"></param>
+    /// <returns></returns>
+    /// 玩家战斗力计算公式一般由策划给出
+    /// 等级*100+物理伤害+法术伤害+物理防御+法术防御
+    public static int GetFightByProps(PlayerData pData)
+    {
+        return pData.lv * 100 + pData.ad + pData.ap + pData.addef + pData.apdef;
+    }
+
+    /// <summary>
+    /// 获取玩家体力值上限
+    /// </summary>
+    /// <param name="lv"></param>
+    /// <returns></returns>
+    /// 计算公式一般由策划提供
+    /// 等级每上涨10级，体力值获得150的上涨；
+    public static int GetPowerLimit(int lv)
+    {
+        return ((lv - 1) / 10) * 150 + 150;
     }
 }
