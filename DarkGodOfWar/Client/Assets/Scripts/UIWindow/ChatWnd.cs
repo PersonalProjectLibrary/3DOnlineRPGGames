9b594b9d0a4daa651fa77db898fc55bcbf1e356e
LoriaRujoy <1306650725@qq.com>
2024-10-09 23:09:26 +0800

    限制聊天消息发送频率

--- DarkGodOfWar/Client/Assets/Scripts/UIWindow/ChatWnd.cs
+++ DarkGodOfWar/Client/Assets/Scripts/UIWindow/ChatWnd.cs
@@ -10,7 +10,9 @@
 #endregion
 
 using PEProtocol;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
@@ -52,6 +54,11 @@ public class ChatWnd : WindowRoot
     private List<string> worldChatList = new List<string>();
 
     /// <summary>
+    /// 能否发送消息
+    /// </summary>
+    private bool canSend = true;
+
+    /// <summary>
     /// 初始化聊天界面
     /// </summary>
     protected override void InitWnd()
@@ -60,6 +67,76 @@ public class ChatWnd : WindowRoot
         chatType = 0;
         RefreshUI();
     }
+
+    #region Click Function
+    /// <summary>
+    /// 点击世界聊天按钮
+    /// </summary>
+    public void ClicWorldkBtn()
+    {
+        audioService.PlayUIAudio(Constants.UiClickBtn);
+        chatType = 0;
+        RefreshUI();
+    }
+
+    /// <summary>
+    /// 点击公会聊天按钮
+    /// </summary>
+    public void ClickGuildBtn()
+    {
+        audioService.PlayUIAudio(Constants.UiClickBtn);
+        chatType = 1;
+        RefreshUI();
+    }
+
+    /// <summary>
+    /// 点击好友聊天按钮
+    /// </summary>
+    public void ClickFriendBtn()
+    {
+        audioService.PlayUIAudio(Constants.UiClickBtn);
+        chatType = 2;
+        RefreshUI();
+    }
+
+    /// <summary>
+    /// 点击发送聊天消息
+    /// </summary>
+    public void ClickSendBtn()
+    {
+        if (canSend)
+        {
+            GameRoot.AddTips("聊天消息每5秒钟才能发送一条");
+            return;
+        }
+        if (iptChat.text == null || iptChat.text == "" || iptChat.text == " ")
+            GameRoot.AddTips("尚未输入聊天信息");
+        else if (iptChat.text.Length > 12) GameRoot.AddTips("输入信息不能超过12个字");
+        else//发送网络消息到服务器
+        {
+            GameMsg msg = new GameMsg
+            {
+                cmd = (int)CMD.SndWorldChat,
+                sndWorldChat = new SndWorldChat { chat = iptChat.text }
+            };
+            iptChat.text = "";
+            netService.SendMsg(msg);
+            canSend = false;
+            StartCoroutine(MsgTimer());//开启定时器，5秒后重置canSend
+        }
+    }
+
+    /// <summary>
+    /// 关闭聊天界面
+    /// </summary>
+    public void ClickCloseBtn()
+    {
+        audioService.PlayUIAudio(Constants.UiClickBtn);
+        chatType = 0;
+        SetWndState(false);
+    }
+
+    #endregion
 
     /// <summary>
     /// 刷新界面UI显示
@@ -98,70 +175,21 @@ public class ChatWnd : WindowRoot
     /// </summary>
     /// <param name="name"></param>
     /// <param name="chat"></param>
-    public void AddWorldChatMsg(string name,string chat)
+    public void AddWorldChatMsg(string name, string chat)
     {
         worldChatList.Add(Constants.SetTxtColor(name + "：", TxtColor.Blue) + chat);
         if (worldChatList.Count > 11) worldChatList.RemoveAt(0);//超过12条，移除第一条
-        if(GetWndState())RefreshUI(); //更新聊天面板的消息显示
+        if (GetWndState()) RefreshUI(); //更新聊天面板的消息显示
     }
 
     /// <summary>
-    /// 点击世界聊天按钮
+    /// 聊天消息间隔计时器
+    /// 发送消息后，隔5秒后重置
     /// </summary>
-    public void ClicWorldkBtn()
+    /// <returns></returns>
+    private IEnumerator MsgTimer()
     {
-        audioService.PlayUIAudio(Constants.UiClickBtn);
-        chatType = 0;
-        RefreshUI();
-    }
-
-    /// <summary>
-    /// 点击公会聊天按钮
-    /// </summary>
-    public void ClickGuildBtn()
-    {
-        audioService.PlayUIAudio(Constants.UiClickBtn);
-        chatType = 1;
-        RefreshUI();
-    }
-
-    /// <summary>
-    /// 点击好友聊天按钮
-    /// </summary>
-    public void ClickFriendBtn()
-    {
-        audioService.PlayUIAudio(Constants.UiClickBtn);
-        chatType = 2;
-        RefreshUI();
-    }
-
-    /// <summary>
-    /// 点击发送聊天消息
-    /// </summary>
-    public void ClickSendBtn()
-    {
-        if (iptChat.text == null || iptChat.text == "" || iptChat.text == " ")
-            GameRoot.AddTips("尚未输入聊天信息");
-        else if (iptChat.text.Length > 12) GameRoot.AddTips("输入信息不能超过12个字");
-        else//发送网络消息到服务器
-        {
-            GameMsg msg = new GameMsg
-            {
-                cmd = (int)CMD.SndWorldChat,
-                sndWorldChat = new SndWorldChat { chat = iptChat.text }
-            };
-            iptChat.text = "";
-            netService.SendMsg(msg);
-        }
-    }
-
-    /// <summary>
-    /// 关闭聊天界面
-    /// </summary>
-    public void ClickCloseBtn()
-    {
-        audioService.PlayUIAudio(Constants.UiClickBtn);
-        chatType = 0;
-        SetWndState(false);
+        yield return new WaitForSeconds(0.5f);
+        canSend = true;
     }
 }
